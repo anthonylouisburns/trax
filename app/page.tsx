09983446cff5021706842eceb395b1a3ea094314
Anthony Louis Burns, Esq <burnstony@gmail.com>
2024-12-13 17:55:24 -0500

    added some notes on connecting to modal

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,13 @@
 
 import { useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
+import Link from "next/link";
 
 export default function Home() {
-  const conversations = useQuery(api.conversations.get);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {conversations?.map(({ _id, when, company, person, desc }) =>
-        <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center gap-x-5" key={_id}>
-          <div className="text-xl font-medium text-green">{company}:{when}:{person}:{desc}</div>
-        </div>
-      )}
+    <main className="">
+      <div><Link href="/conversations" className="hover:underline hover:text-blue-600">conversations</Link></div>
+      <div><Link href="/outputfiles" className="hover:underline hover:text-blue-600">files</Link></div>
     </main>
   );
 }
--- modal/README.md
+++ modal/README.md
@@ -4,3 +4,16 @@ curl --request GET \
 
 curl 'https://anthonylouisburns--has-simple-web-endpoint-square.modal.run/?x=8' 
 <!--  3.13.0/envs/modal-python-3 -->
+
+https://modal.com/docs/reference/modal.config#modaltoml
+## modal.config
+Modal intentionally keeps configurability to a minimum.
+
+The main configuration options are the API tokens: the token id and the token secret. These can be configured in two ways:
+
+1. By running the modal token set command. This writes the tokens to .modal.toml file in your home directory.
+2. By setting the environment variables MODAL_TOKEN_ID and MODAL_TOKEN_SECRET. This takes precedence over the previous method.
+
+modal token set --token-id - --token-secret - --profile=dealstart
+modal profile activate dealstart
+modal profile activate anthonylouisburns
